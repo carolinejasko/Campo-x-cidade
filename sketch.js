02e534991ee2916bba4d8c416b664b798e7f48c9
carolinejasko <caroline.jasko@escola.pr.gov.br>
2025-06-11 11:12:09 -0300

    campo x cidade

---
+++ sketch.js
@@ -0,0 +1,59 @@
+   //jogo cidae x campo usando as teclas a e s
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+let xJogador = [0, 0,];
+let yJogador = [ 150, 225];
+let jogador = ["🐄", "🌆"];
+let teclas = ["a", "s"];
+let quantidade = jogador.length;
+
+function draw() {
+  ativaJogo();
+  desenhaJogadores();
+  desenhaLinhaDeChegada();
+  verificaVencedor();
+}
+
+function ativaJogo() {
+  if (focused == true) {
+    background("#7C7C7C");
+  } else {
+    background("#376B39");
+  }
+}
+
+function desenhaJogadores() {
+  textSize(40);
+  for (let i = 0; i < quantidade; i++) {
+    text(jogador[i], xJogador[i], yJogador[i]);
+  }
+}
+
+function desenhaLinhaDeChegada() {
+  fill("white");
+  rect(350, 0, 10, 400);
+  fill("black");
+  for (let yAtual = 0; yAtual < 400; yAtual += 20) {
+    rect(350, yAtual, 10, 10);
+  }
+}
+
+function verificaVencedor() {
+  for (let i = 0; i < quantidade; i++) {
+    if (xJogador[i] > 350) {
+      text(jogador[i] + " venceu!", 50, 200);
+      noLoop();
+    }
+  }
+}
+
+function keyReleased() {
+  for (let i = 0; i < quantidade; i++) {
+    if (key == teclas[i]) {
+      xJogador[i] += random(20);
+    }
+  }
+}
